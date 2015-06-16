d927c95d27f9bd3becffdc2a1feb64f15a221f81
Sean Renet <goodstuff2read@gmail.com>
2015-06-16 16:57:34 -0700

    Added a Test Class

---
+++ Git Test/GitTestFolder/TestClass.h
@@ -0,0 +1,13 @@
+//
+//  TestClass.h
+//  Git Test
+//
+//  Created by Apple Apps on 6/16/15.
+//  Copyright (c) 2015 Argo Media International. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass : NSObject
+
+@end
---
+++ Git Test/GitTestFolder/TestClass.m
@@ -0,0 +1,13 @@
+//
+//  TestClass.m
+//  Git Test
+//
+//  Created by Apple Apps on 6/16/15.
+//  Copyright (c) 2015 Argo Media International. All rights reserved.
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+
+@end
